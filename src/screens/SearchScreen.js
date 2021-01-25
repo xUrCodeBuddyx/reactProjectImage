6e30536a714930b7865cef082bb8eacc9edf29e6
Sami Hedhli <59134280+Sami-Hedhli@users.noreply.github.com>
2021-01-25 19:00:19 +0100

    improved pagination

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -6,12 +6,12 @@ import ImageList from "../components/ImageList";
 import axios from "axios";
 
 const SearchScreen = () => {
-  const [term,setTerm] = useState();
+  const [term, setTerm] = useState();
   const [images, setImages] = useState([]);
   const [page, setPage] = useState(1);
-
+  let p = 1;
   let onSearchSubmit = async (value) => {
-    setTerm(value)
+    setTerm(value);
     const response = await axios.get("https://pixabay.com/api/", {
       params: {
         key: "17886991-9ee3d7544c84f90d0e8126941",
@@ -19,24 +19,22 @@ const SearchScreen = () => {
         page: page
       }
     });
-    console.log(response.data);
-    console.log(term);
 
     setImages(response.data.hits);
   };
 
   return (
     <View>
-      <SearchBar  onChange={onSearchSubmit} />
+      <SearchBar onChange={onSearchSubmit} />
       <View>
         <Button
-          title={page}
-          onPress={(e) => { 
+          title={`Page: ${page}`}
+          onPress={() => {
             setPage(page + 1);
             onSearchSubmit(term);
           }}
         />
-        <ImageList images={images} page={page} />
+        <ImageList images={images} page={p} />
       </View>
     </View>
   );
